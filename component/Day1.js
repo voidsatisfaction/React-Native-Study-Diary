4c67c1f1a7b2830dc3e8588a344ab6aad9c0e3d5
voidSatisfaction <lourie@naver.com>
2016-08-26 18:20:30 +0900

    Add function to day1

--- component/Day1.js
+++ component/Day1.js
@@ -10,31 +10,48 @@ import { Actions } from 'react-native-router-flux';
 
 export default class Stopwatch extends Component {
   componentWillMount() {
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
     this.setState({
       mainTime: 0,
       subTime: 0,
+      totalMainTime: '00 : 00 : 00',
+      totalSubTime: '00 : 00 : 00',
       timerOn: false,
-      dataSource: ds.cloneWithRows(['row1', 'row2']),
+      btnNameL: 'Start',
+      btnNameR: 'Stop',
+      btnFuncL: this._startTime,
+      btnFuncR: this._stopTime,
+      rowData: [
+        {title: '', record: ''},
+        {title: '', record: ''},
+        {title: '', record: ''},
+        {title: '', record: ''}
+      ],
+      recordCounter: 1,
     })
   }
 
   render() {
-    console.log(this.state)
+    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    theDataSource = ds.cloneWithRows(this.state.rowData)
     return (
       <View style={day1Style.container}>
         <View style={{height: 60}}></View>
-        <Header mainTime={this.state.mainTime}/>
+        <Header
+          totalMainTime={this.state.totalMainTime}
+          totalSubTime={this.state.totalSubTime}
+          timeSeperation={this._timeSeperation.bind(this)}
+        />
         <Buttons
-          startTime={this._startTime}
-          stopTime={this._stopTime}
-          stopWatch={this}/>
+          btnFuncL={this.state.btnFuncL.bind(this)}
+          btnFuncR={this.state.btnFuncR.bind(this)}
+          btnNameL={this.state.btnNameL}
+          btnNameR={this.state.btnNameR}
+        />
         <ListView
-          dataSource={this.state.dataSource}
+          dataSource={theDataSource}
           renderRow={this._renderRow}
           initialListSize={4}
         />
-        <Text>{this.state.mainTime}</Text>
       </View>
     );
   }
@@ -42,42 +59,109 @@ export default class Stopwatch extends Component {
   _renderRow(rowData) {
     return (
       <View style={day1Style.timeline}>
-        <Text>{rowData}</Text>
+        <Text>{rowData.title + " >> " + rowData.record}</Text>
       </View>
-    )
-  }
-
-  _genRow() {
-    return (
-      "helllo"
     )
   }
 
   _startTime(){
     if (!this.state.timerOn){
-      var timer = setInterval(() => {
+      let timer = setInterval(() => {
         let mainTime = this.state.mainTime
         let subTime = this.state.subTime
+        let totalMainTime = this._timeSeperation(mainTime)
+        let totalSubTime = this._timeSeperation(subTime)
 
         this.setState({
+          totalMainTime: totalMainTime,
+          totalSubTime: totalSubTime,
           mainTime: mainTime + 1,
           subTime: subTime + 1,
         })
-      }, 100)
+      }, 10)
 
       this.setState({
         timer: timer,
         timerOn: true,
+        btnNameL: 'Register',
+        btnNameR: 'Stop',
+        btnFuncL: this._registerTime,
+        btnFuncR: this._stopTime,
       })
     }
   }
 
   _stopTime() {
-    clearInterval(this.state.timer)
+    if (this.state.timerOn) {
+      clearInterval(this.state.timer)
+
+      this.setState({
+        timerOn: false,
+        btnNameL: 'Start',
+        btnNameR: 'Reset',
+        btnFuncL: this._startTime,
+        btnFuncR: this._clearTime,
+      })
+    }
+  }
+
+  _registerTime() {
+    let rowData = this.state.rowData
+    let recordCounter = this.state.recordCounter
+    let totalMainTime = this.state.totalMainTime
+
+    rowData.unshift({
+      title: 'Number ' + this.state.recordCounter,
+      record: totalMainTime,
+    })
 
     this.setState({
-      timerOn: false,
+      rowData: rowData,
+      recordCounter: ++recordCounter,
+      subTime: 0,
     })
+  }
+
+  _clearTime() {
+    this.setState({
+      mainTime: 0,
+      subTime: 0,
+      totalMainTime: '00 : 00 : 00',
+      totalSubTime: '00 : 00 : 00',
+      btnNameL: 'Start',
+      btnNameR: 'Stop',
+      btnFuncL: this._startTime,
+      btnFuncR: this._stopTime,
+      rowData: [
+        {title: '', record: ''},
+        {title: '', record: ''},
+        {title: '', record: ''},
+        {title: '', record: ''}
+      ],
+      recordCounter: 1,
+    })
+  }
+
+  _timeSeperation(time) {
+    if (time >= 360000) {
+      var hours = Math.floor(time / 360000)
+      time -= hours * 360000
+
+      var minutes = Math.floor(time / 6000)
+      time -= minutes * 6000
+
+      var seconds = Math.floor(time / 100)
+
+      return(`${hours} : ${minutes<10 ? '0'+minutes : minutes} : ${seconds<10 ? '0'+seconds : seconds}`)
+    } else {
+      var minutes = Math.floor(time / 6000)
+      time -= minutes * 6000
+
+      var seconds = Math.floor(time / 100)
+      var miliseconds = time - seconds * 100
+
+      return(`${minutes<10 ? '0'+minutes : minutes} : ${seconds<10 ? '0'+seconds : seconds} : ${miliseconds<10 ? '0'+miliseconds : miliseconds}`)
+    }
   }
 }
 
@@ -86,10 +170,10 @@ class Header extends Component {
     return (
       <View style={day1Style.header}>
         <Text>
-          12:34.56
+          {this.props.totalSubTime}
         </Text>
         <Text>
-          {this.props.mainTime}
+          {this.props.totalMainTime}
         </Text>
       </View>
     )
@@ -102,16 +186,16 @@ class Buttons extends Component {
       <View style={day1Style.buttons}>
         <TouchableOpacity
           style={day1Style.button}
-          onPress={this.props.startTime.bind(this.props.stopWatch)}>
+          onPress={this.props.btnFuncL}>
           <Text>
-            start
+            {this.props.btnNameL}
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={day1Style.button}
-          onPress={this.props.stopTime.bind(this.props.stopWatch)}>
+          onPress={this.props.btnFuncR}>
           <Text>
-            End
+            {this.props.btnNameR}
           </Text>
         </TouchableOpacity>
       </View>
